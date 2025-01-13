9fb92fc855b13c107c0e532030a4d447e5b60fe9
thiagocardoso1 <cardosot204@gmail.com>
2025-01-13 18:42:44 -0300

    final commit for calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,15 @@
 export default function Button(props) {
-    return (
-        <button className="button">{props.label}</button>
-    )
-} 
+  return (
+    <button
+      className={`
+        button
+        ${props.operation ? "operation" : ""}
+        ${props.double ? "double" : ""}
+        ${props.triple ? "triple" : ""}
+      `}
+      onClick={(_) => props.click && props.click(props.label)}
+    >
+      {props.label}
+    </button>
+  );
+}
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,27 +1,105 @@
 import Button from "../components/Button";
 import Display from "../components/Display";
+import { useState } from "react";
+
+const inicialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
 
 export default function Calculator() {
+  const [state, setState] = useState({ ...inicialState });
+
+  function clearMemory() {
+    setState({ ...inicialState });
+  }
+
+  function setOperation(operation) {
+    if (state.current === 0) {
+      setState((prevState) => ({
+        ...prevState,
+        operation: operation,
+        current: 1,
+        clearDisplay: true,
+      }));
+    } else {
+      const equals = operation === "=";
+      const currentOperation = state.operation;
+
+      const values = [...state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+        if (isNaN(values[0]) || !isFinite(values[0])) {
+          clearMemory();
+          return;
+        }
+      } catch (e) {
+        values[0] = state.values[0];
+      }
+      values[1] = 0;
+
+      setState((prevState) => ({
+        ...prevState,
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values: values,
+      }));
+    }
+  }
+
+  function addDigit(n) {
+    if (n === "." && state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay = state.displayValue === "0" || state.clearDisplay;
+    const currentValue = clearDisplay ? "" : state.displayValue;
+    const displayValue = currentValue + n;
+
+    setState((prevState) => ({
+      ...prevState,
+      displayValue: displayValue,
+      clearDisplay: false,
+    }));
+
+    if (n !== ".") {
+      const i = state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...state.values];
+      values[i] = newValue;
+
+      setState((prevState) => ({
+        ...prevState,
+        values: values,
+      }));
+    }
+  }
+
   return (
     <main className="calculator">
-      <Display value={100}/>
-      <Button label="AC" />
-      <Button label="/" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="*" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
-      <Button label="." />
-      <Button label="=" />
+      <Display value={state.displayValue} />
+      <Button label="AC" click={clearMemory} triple />
+      <Button label="/" click={setOperation} operation />
+      <Button label="7" click={addDigit} />
+      <Button label="8" click={addDigit} />
+      <Button label="9" click={addDigit} />
+      <Button label="*" click={setOperation} operation />
+      <Button label="4" click={addDigit} />
+      <Button label="5" click={addDigit} />
+      <Button label="6" click={addDigit} />
+      <Button label="-" click={setOperation} operation />
+      <Button label="1" click={addDigit} />
+      <Button label="2" click={addDigit} />
+      <Button label="3" click={addDigit} />
+      <Button label="+" click={setOperation} operation />
+      <Button label="0" click={addDigit} double />
+      <Button label="." click={addDigit} />
+      <Button label="=" click={setOperation} operation />
     </main>
   );
 }
